ad134f6025b87762e3efda4bf24652981fba7751
JasonYoo1 <jyoojs@gmail.com>
2019-06-20 14:30:35 -0700

    fixed bugs. MVP

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -26,7 +26,7 @@ class TabLink {
     const links = document.querySelector('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(link => this.link.classList.remove('tabs-link-selected'));
+    Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
   
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
@@ -45,12 +45,14 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    const items = document.querySelector('tabs-item');
+    const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    Array.from(links).forEach(link => this.link.classList.remove('tabs-link-selected'));
+    Array.from(items).forEach(link => link.classList.remove('tabs-item-selected'));
+
     // Add a class named "tabs-item-selected" to this element
-    this.element.classList.add('tabs-link-selected');  }
+    this.element.classList.add('tabs-item-selected');  
+  }
 }
 
 /* START HERE: 
